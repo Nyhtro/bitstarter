081d0525ccf8827fa19e61cb17d085e6f686ae03
Trond Martin Nyhus <nyhtro@gmail.com>
2013-07-10 20:07:57 +0000

    minor bugfix

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 var fs = require('fs');
 
-App.get('/', function(request, response) {
+app.get('/', function(request, response) {
     // response.send('Hello World 2!');
     var output = fs.readFileSync('index.html').toString();
     // console.log("Ze output " + output);
